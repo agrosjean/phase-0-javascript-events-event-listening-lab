fa416302e0e4c531936cd04d916ccf27c2a70420
alexandre <agrosjean007@gmail.com>
2021-10-18 17:09:54 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function addingEventListener() {
     const input = document.getElementById('input');
 
     function clickAlert() {
-        alert('Hee hee, iwas tickled');
+        alert('I was clicked');
     }
 
     input.addEventListener('click', clickAlert);
